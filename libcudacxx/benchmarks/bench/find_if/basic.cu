aac943d19c7f638da274311cc76d7e699d96ee1f
Surya! <thedevmystic@gmail.com>
2026-02-26 18:24:04 +0530

    feat: add central equal to value predicate (#7496)
    Unify the different implementations throughout the project and make it publicly available in the cuda namespace

--- libcudacxx/benchmarks/bench/find_if/basic.cu
+++ libcudacxx/benchmarks/bench/find_if/basic.cu
@@ -10,26 +10,12 @@
 
 #include <thrust/device_vector.h>
 
+#include <cuda/functional>
 #include <cuda/memory_pool>
 #include <cuda/std/__pstl_algorithm>
 #include <cuda/stream>
 
 #include "nvbench_helper.cuh"
-
-template <class T>
-struct equal_to_val
-{
-  T val_;
-
-  constexpr equal_to_val(const T& val) noexcept
-      : val_(val)
-  {}
-
-  __device__ constexpr bool operator()(const T& val) const noexcept
-  {
-    return val == val_;
-  }
-};
 
 template <typename T>
 static void basic(nvbench::state& state, nvbench::type_list<T>)
@@ -49,10 +35,11 @@ static void basic(nvbench::state& state, nvbench::type_list<T>)
 
   caching_allocator_t alloc{};
 
-  state.exec(
-    nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch | nvbench::exec_tag::sync, [&](nvbench::launch& launch) {
-      do_not_optimize(cuda::std::find_if(cuda_policy(alloc, launch), dinput.begin(), dinput.end(), equal_to_val{val}));
-    });
+  state.exec(nvbench::exec_tag::gpu | nvbench::exec_tag::no_batch | nvbench::exec_tag::sync,
+             [&](nvbench::launch& launch) {
+               do_not_optimize(
+                 cuda::std::find_if(cuda_policy(alloc, launch), dinput.begin(), dinput.end(), cuda::equal_to_val{val}));
+             });
 }
 
 NVBENCH_BENCH_TYPES(basic, NVBENCH_TYPE_AXES(fundamental_types))
